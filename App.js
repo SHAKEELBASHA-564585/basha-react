78a15852e21b8009239db1104d3a934499fd2f41
basha <luckychinnu585564@gmail.com>
2023-09-28 19:16:22 +0530

    commiting my files

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from "react-dom/client"
 
 const head1=React.createElement("h1",{},"Namasthe Everone From React basha!");
-const head2=React.createElement('h2',{},"Namaste raa  Basha")
+const head2=React.createElement('h2',{},"Helllo raa Basha")
 const container=React.createElement("div",{
     id:"container"
 },[head1,head2])
