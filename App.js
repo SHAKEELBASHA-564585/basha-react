060371210ed5d2c6738e7ecfbea6062322b74264
basha <luckychinnu585564@gmail.com>
2023-10-02 09:39:50 +0530

    introduced prettier

--- App.js
+++ App.js
@@ -1,30 +1,24 @@
-
 import React from 'react'
-import ReactDOM from "react-dom/client"
+import ReactDOM from 'react-dom/client'
 
-const Heading=()=>(
-    <h1>Basha</h1>
-)    
+const Heading = () => <h1>Basha</h1>
 
-const a=10;
-const Basha=()=>{
-    return(
-        <>
-            <div>
-        
-            <Heading/>
-            {console.log(5
-            )}
-                <h1>I want to learn ReactJS</h1>
-                <p>I want to put more concentration on this.</p>
-            </div>
-            <ul>
-                <li>learn Redux</li>
-                <li>Learn Routing</li>
-            </ul>
-        </>
-    )  
+const a = 10
+const Basha = () => {
+  return (
+    <>
+      <div>
+        <Heading />
+        {console.log(5)}
+        <h1>I want to learn ReactJS</h1>
+        <p>I want to put more concentration on this.</p>
+      </div>
+      <ul>
+        <li>learn Redux</li>
+        <li>Learn Routing</li>
+      </ul>
+    </>
+  )
 }
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Basha/>);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Basha />)
