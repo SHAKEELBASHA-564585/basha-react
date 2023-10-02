17a25b8b035e4cc7490814d1b216a16158892983
basha <luckychinnu585564@gmail.com>
2023-10-02 07:01:02 +0530

    Adding new topics like JSX to my project

--- App.js
+++ App.js
@@ -2,10 +2,29 @@
 import React from 'react'
 import ReactDOM from "react-dom/client"
 
-const head1=React.createElement("h1",{},"Namasthe Everone From React basha!");
-const head2=React.createElement('h2',{},"Helllo raa Basha")
-const container=React.createElement("div",{
-    id:"container"
-},[head1,head2])
+const Heading=()=>(
+    <h1>Basha</h1>
+)    
+
+const a=10;
+const Basha=()=>{
+    return(
+        <>
+            <div>
+        
+            <Heading/>
+            {console.log(5
+            )}
+                <h1>I want to learn ReactJS</h1>
+                <p>I want to put more concentration on this.</p>
+            </div>
+            <ul>
+                <li>learn Redux</li>
+                <li>Learn Routing</li>
+            </ul>
+        </>
+    )  
+}
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<Basha/>);
