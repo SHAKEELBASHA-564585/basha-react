c6112f79ef3cd52c55b64474f5d354cc5b1a89f4
basha <luckychinnu585564@gmail.com>
2023-10-06 08:44:51 +0530

    Just Added Some CSS

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,35 +2,30 @@ import { useState } from "react";
 import { restaurants } from "../../constants";
 import Card from "./Card"
 
-
-const filterData = (searchInput, hotels) => {
-    const finalData = hotels.filter((each) => each.name.includes(searchInput))
+const filterData = (searchInput, hotels) => { // filtering the hotels
+    const finalData = hotels.filter((each => each.name.includes(searchInput)))
     return finalData
 }
 
 const Body = () => {
 
-    const [searchInput, setSearchInput] = useState();
-    const [hotels, setHotels] = useState(restaurants)
+    const [searchInput, setSearchInput] = useState();   //var,callback function
+    const [hotels, setHotels] = useState(restaurants);  //var,callback function
 
     return (
         <>
             <div className="search-cont">
-                <input type="text"
-                    className="search-input"
-                    placeholder="Search"
-                    value={searchInput}
-                    onChange={(e) => {
-                        setSearchInput(e.target.value)
-                    }}
-                />
+                <input type="text" className="search-bar" placeholder="Search" value={searchInput} onChange={(e) => {
+                    setSearchInput(e.target.value)
+                }} />
 
                 <button className="btn" onClick={() => {
-                    const data = filterData(searchInput, hotels)
-                    setHotels(data)
+                    const data = filterData(searchInput, hotels) //filtered data according to user input
+                    setHotels(data) //returned data
                 }}>Search</button>
 
             </div>
+
             <div className="card-list">
                 {hotels.map(each => {
                     return <Card {...each} key={each.id} />
